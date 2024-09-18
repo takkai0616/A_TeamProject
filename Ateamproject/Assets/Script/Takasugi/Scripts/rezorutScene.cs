9eeaac39c073df9aac3d693c8eba5a444ce5c140
kaito <takkai.6161705@gmail.com>
2024-09-18 15:34:23 +0900

    シーン遷移

---
+++ Ateamproject/Assets/Script/Takasugi/Scripts/rezorutScene.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// タイトル画面の進行を制御します。
+public class rezorutScene: MonoBehaviour
+{
+    // 次のシーンを読み込み可能な場合はtrue、それ以外はfalse
+    bool isLoadable = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+      
+    }
+
+    // ２秒待機後に次のシーンを読込み可能にします。
+    IEnumerator OnStart()
+    {
+        yield return new WaitForSeconds(2);
+        isLoadable = true;
+    }
+
+    // 決定ボタン操作の際に呼び出され、次のシーンを読み込みます。
+    public void LoadNextScene()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            StartCoroutine(OnStart());
+        }
+
+        if (isLoadable)
+        {
+            SceneManager.LoadScene("Title Scene");
+        }
+    }
+}
