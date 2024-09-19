936156aa791a51072a4345e22c46fbe41a78eeeb
kaito <takkai.6161705@gmail.com>
2024-09-19 14:42:12 +0900

    no message

--- Ateamproject/Assets/Script/Takasugi/Scripts/TitleScene.cs
+++ Ateamproject/Assets/Script/Takasugi/Scripts/TitleScene.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 // タイトル画面の進行を制御します。
 public class TitleScene : MonoBehaviour
 {
     // 次のシーンを読み込み可能な場合はtrue、それ以外はfalse
     bool isLoadable = false;
 
+
+    bool Serect = true;
+
+    [SerializeField]
+    GameObject serect;
+
+    [SerializeField]
+    GameObject serect2;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,14 +39,51 @@ public class TitleScene : MonoBehaviour
 
     private void Update()
     {
+        
+
+
+
+
+        if (Serect == true)
+        {   //ゲーム開始
+
+            //選択の表示変えたい
+            serect.SetActive(true);
+            serect2.SetActive(false);
+
+            if (isLoadable)
+            {
+                SceneManager.LoadScene("MainScene");
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow)) 
+            { Serect = false; }
+        }
+
+        if (Serect == false)
+        {   //終了
+
+            //選択の表示変えたい
+            serect.SetActive(false);
+            serect2.SetActive(true);
+
+            if (isLoadable)
+            {
+                Application.Quit();
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            { Serect = true; }
+        }
+
+
+
+        //決定
         if (Input.GetKey(KeyCode.Return))
         {
             StartCoroutine(OnStart());
         }
 
-        if (isLoadable)
-        {
-            SceneManager.LoadScene("MainScene");
-        }
+        
     }
 }
