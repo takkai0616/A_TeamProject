dca114efc76062cc1774b113f387127ba5b09abd
kaito <takkai.6161705@gmail.com>
2024-09-20 17:09:48 +0900

    若松　effect修正

--- Ateamproject/Assets/Script/Wakamatu/FootPrintSmall.cs
+++ Ateamproject/Assets/Script/Wakamatu/FootPrintSmall.cs
@@ -6,11 +6,6 @@ public class FootPrintSmall : MonoBehaviour
 {
     [SerializeField] Player player;
     [SerializeField]
-    private float x;
-    [SerializeField]
-    private float y;
-    [SerializeField]
-    private float z;
     public GameObject footPrintPrefab;
     bool create;
     void Start()
@@ -19,7 +14,6 @@ public class FootPrintSmall : MonoBehaviour
     }
     void Update()
     {
-        gameObject.transform.rotation = Quaternion.Euler(x, y, z);
         if (player.isRotate && create)
         {
             Instantiate(footPrintPrefab, transform.position, transform.rotation);
